18c584d0c94b562ee6c23b2b47b59f68bfe75afa
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-20 14:39:37 -0400

    advanced object

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,71 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |song_name|
+      song_name.name == name
+      
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      self.all
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+
+  end
+
+  def self.new_from_filename(filename)
+    file_array = filename.split(/[-.]/)
+    song = self.new
+    song.name = file_array[1].strip
+    song.artist_name = file_array[0].strip
+    song
+  end
+
+  def self.create_from_filename(filename)
+    file_array = filename.split(/[-.]/)
+    song = self.new
+    song.name = file_array[1].strip
+    song.artist_name = file_array[0].strip
+    song.save
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
